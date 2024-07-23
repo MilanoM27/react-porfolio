ad26aa845312cf9c118c00b7951c815aab728d48
MilanoM <60301624+MilanoM27@users.noreply.github.com>
2024-07-23 16:07:11 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -52,7 +52,7 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Transforming Concepts into Next level User Experiences"
+            words="Turning Concepts into Next Level User Experiences"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
